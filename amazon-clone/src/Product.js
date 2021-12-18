9a940eafaf339ae0d76b7907116b7cc25dd0d1b3
Joe <joeqiusc@gmail.com>
2021-12-17 21:18:47 -0500

    fixed the basket and dipatch

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,15 +1,33 @@
-import React from 'react'
-import './Product.css'
-function Product ({ title, image, price, rating }) {
+import React from "react";
+import "./Product.css";
+import { useStateValue } from "./StateProvider";
+
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  // console.log("This is the basket=>>>", basket);
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
-    <div className='product'>
-      <div className='product__info'>
+    <div className="product">
+      <div className="product__info">
         <p>{title}</p>
-        <p className='product__price'>
+        <p className="product__price">
           <small>$</small>
           <strong>{price}</strong>
         </p>
-        <div className='product__rating'>
+        <div className="product__rating">
           {Array(rating)
             .fill()
             .map((_, i) => (
@@ -17,10 +35,12 @@ function Product ({ title, image, price, rating }) {
             ))}
         </div>
       </div>
-      <img src={image} alt='' />
-      <button>Add to Basket</button>
+
+      <img src={image} alt="" />
+
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
